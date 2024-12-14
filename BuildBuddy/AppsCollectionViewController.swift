ded940bbad0cabcabc9b0444a2f64eac7e79bd8f
dreamlogin <dreamlogin@vk.team>
2024-12-14 21:55:37 +0300

    Инициализация проекта

---
+++ BuildBuddy/AppsCollectionViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class AppsCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
